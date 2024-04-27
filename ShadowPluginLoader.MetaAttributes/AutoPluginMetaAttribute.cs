40dae25647c4ac7265876b6b981afefa92875ce0
Nacho <kulujun@gmail.com>
2024-04-28 00:35:31 +0800

    update

---
+++ ShadowPluginLoader.MetaAttributes/AutoPluginMetaAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace ShadowPluginLoader.MetaAttributes;
+
+/// <summary>
+/// Auto Import PluginMeta
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class AutoPluginMetaAttribute : Attribute
+{
+}

9b44fc3a1d9a906e28f6325ba4dd4f255e307ec6
kituin <kit_uin@outlook.com>
2025-04-07 16:00:00 +0800

    Fix | Read value

--- TestProject1/obj/Debug/net8.0-windows/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0-windows/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16952c98082228a1ca93fcff1f846ede57fbf72c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66405bfc52bcd6c51676b1a91a24636f3cb2b596")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

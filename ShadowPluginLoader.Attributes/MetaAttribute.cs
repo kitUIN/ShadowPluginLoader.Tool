7b2c4d5473f869542837575e3376851e84383bcb
kitUIN <kulujun@gmail.com>
2025-04-14 17:49:47 +0800

    Feat | Type in plugin.json

--- ShadowPluginLoader.Attributes/MetaAttribute.cs
+++ ShadowPluginLoader.Attributes/MetaAttribute.cs
@@ -1,15 +1,16 @@
 ﻿namespace ShadowPluginLoader.Attributes;
+
 /// <summary>
 /// Used to mark a property as a meta attribute.
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
-public class MetaAttribute: Attribute
+public class MetaAttribute : Attribute
 {
     /// <summary>
     /// Is Required
     /// </summary>
     public bool Required { get; init; } = true;
-    
+
     /// <summary>
     /// Is Excluded
     /// </summary>
@@ -19,14 +20,24 @@ public class MetaAttribute: Attribute
     /// Mapping Project PropertyGroup Value
     /// </summary>
     public string? PropertyGroupName { get; init; }
-    
+
     /// <summary>
     /// Using Regex To Validate
     /// </summary>
     public string? Regex { get; init; }
-    
+
     /// <summary>
     /// EntryPoint Name
     /// </summary>
     public string? EntryPointName { get; init; }
+
+    /// <summary>
+    /// Type in plugin.json
+    /// </summary>
+    public Type? JsonType { get; init; }  
+    
+    /// <summary>
+    /// Construction Template
+    /// </summary>
+    public string? ConstructionTemplate { get; init; }
 }
